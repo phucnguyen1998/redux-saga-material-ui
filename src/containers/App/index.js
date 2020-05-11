9f302836045acef3bc636c9f37048ab2ca331a6c
phucnguyen1998 <phucnguyen11011998@gmail.com>
2020-05-11 16:40:18 +0700

    create db, store

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -4,15 +4,20 @@ import { withStyles } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/core/styles';
 import TaskBoard from '../TaskBoard';
 import theme from '../../commons/Theme';
+import { Provider } from 'react-redux';
+import configStore from './../../redux/configStore';
 
+const store = configStore();
 
 class App extends Component {
 
   render() {
     return (
-      <ThemeProvider theme={theme}>
-        <TaskBoard/>
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <TaskBoard/>
+        </ThemeProvider>
+      </Provider>
     );
   }
 }
