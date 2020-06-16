62909710f829eb7ba5b7eab073e94ecdc32ac96d
Phúc Nguyễn <mac@MacBook-Pro-cua-mac.local>
2020-06-16 11:22:25 +0700

    update fillter task

--- src/apis/task.js
+++ src/apis/task.js
@@ -1,11 +1,16 @@
 import axiosService from './../commons/axiosServices';
 import { API_ENDPOINT } from './../constants';
+import qs from 'query-string';
 
 //  http://localhost:3000/tasks
 const url = 'tasks';
 
-export const getList = () => {
-  return axiosService.get(`${API_ENDPOINT}/${url}`);
+export const getList = (params = {}) => {
+  let queryParams = '';
+  if(Object.keys(params).length > 0){
+    queryParams = `?${qs.stringify(params)}`;
+  }
+  return axiosService.get(`${API_ENDPOINT}/${url}${queryParams}`);
 }
 
 export const addTask = (data) => {
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,8 +1,8 @@
-import { fork,take,call,put,delay,takeLatest,select,takeEvery } from 'redux-saga/effects';
+import { fork,take,call,put,delay,takeLatest,takeEvery } from 'redux-saga/effects';
 import * as taskType from './../constants/task';
 import { getList,addTask } from './../apis/task';
 import { STATUS_CODE, STATUSES } from './../constants/index';
-import { fetchListTaskSuccess, fetchListTaskFailed,filterTaskSuccess, addTaskSuccess, addTaskFailed } from '../actions/task';
+import { fetchListTask,fetchListTaskSuccess, fetchListTaskFailed, addTaskSuccess, addTaskFailed } from '../actions/task';
 import {showLoading,hideLoading} from './../actions/UI';
 import {hideModal} from './../actions/modal';
 
@@ -19,9 +19,13 @@ import {hideModal} from './../actions/modal';
 
 function* watchFetchListTaskAction() {
   while(true){
-    yield take(taskType.FETCH_TASK);
+    const action = yield take(taskType.FETCH_TASK);
     yield put(showLoading());
-    let res = yield call(getList);
+    //console.log('action', action);
+    const { params } = action.payload; 
+    //console.log('action', params);
+    
+    let res = yield call(getList,params);
     const {status, data} = res;
     if(status === STATUS_CODE.SUCSESS){
       // dispatch action fetchListTaskSuccess
@@ -39,11 +43,9 @@ function* watchFetchListTaskAction() {
 function* filterTaskSaga({payload}) {
   yield delay(500);
   const { keyword } = payload;
-  let list = yield select(state => state.task.listTask);
-  const filteredTask = list.filter(task => task.title.trim().toLowerCase().includes(keyword.trim().toLowerCase()));
-  console.log(filteredTask);
-  
-  yield put(filterTaskSuccess(filteredTask));
+  yield put(fetchListTask({
+    q:keyword
+  }))
   
 }
 
